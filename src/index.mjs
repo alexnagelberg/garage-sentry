7c4e335133c634b0361f3a8ad04d27a5e1df4d06
Alexander Nagelberg <alex@alexnagelberg.com>
2025-10-16 15:13:11 -0500

    Sends notification to app, uses config, added separate util for pushing webcam snapshots only

--- src/index.mjs
+++ src/index.mjs
@@ -2,38 +2,64 @@ import { GoogleGenAI, Type } from '@google/genai'
 import { exec } from 'node:child_process'
 import { readFileSync } from 'fs'
 import { importPKCS8, SignJWT } from 'jose'
-
+import { pushoverKey, pushoverUserKeys, genApiKey, workingDirectory, imagePath, model, host } from './config.mjs'
 import axios from 'axios'
 
-// TODO: put in cron
+const ai = new GoogleGenAI({ apiKey: genApiKey })
 
-const apiKey = ''
+const sendNotification = async (message, image) => {
+  const alg = 'RS256'
+  const privateKey = await importPKCS8(readFileSync(`${workingDirectory}/private.key`).toString(), alg)
+  const jwt = await new SignJWT({ prop: 'value' })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime('1 minute')
+    .sign(privateKey)
+  await axios.post(
+    `https://${host}/api/garage/push/notify`,
+    { message },
+    { headers: { Authorization: `Bearer ${jwt}` } }
+  )
+  /*for await (const userKey of pushoverUserKeys) {
+    try {
+      await axios.post('https://api.pushover.net/1/messages.json', {
+        token: pushoverKey,
+        user: userKey,
+        message,
+        attachment_type: 'image/jpeg',
+        attachment_base64: image,
+        url: 'https://nagelberg.dev/garage'
+      })
+    } catch (e) {
+      console.error('Error sending pushover notification', e)
+    }
+  }*/
+}
 
-const ai = new GoogleGenAI({ apiKey })
-const imagePath = '/tmp/garage.jpg'
+//const run = async () => {
 exec(`fswebcam -r 1280x720 --no-banner ${imagePath}`, async err => {
-  const base64ImageFile = readFileSync(imagePath, {
-    encoding: 'base64'
-  })
+  const currentSnapshot = readFileSync(imagePath, { encoding: 'base64' })
 
   const contents = [
     {
       inlineData: {
         mimeType: 'image/jpeg',
-        data: base64ImageFile
+        data: currentSnapshot
       }
     },
-    //{ text: 'Do you see a person or not?' }
     //    { text: 'Do you see a garage door open or not (true for open/sunlight coming through, false for all closed or pitch black)? How many cars do you see parked (if pitch black, respond with 2)?' }
     {
-      text: 'Do you see a garage door open or not on the left and on the right (true for open/sunlight coming through, false for closed or pitch black)? Do you see a car parked on the left and on the right (true for both if pitch black)'
+      text: 'Do you see a garage door fully open or not on the left? on the right (true for open wih outside coming through, false for closed or room is pitch black)? Do you see a car parked on the left and on the right (true for both if pitch black)?'
     }
   ]
 
   const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
+    model,
     contents,
     config: {
+      thinkingConfig: {
+        thinkingBudget: -1
+      },
       responseMimeType: 'application/json',
       responseSchema: {
         type: Type.OBJECT,
@@ -57,20 +83,41 @@ exec(`fswebcam -r 1280x720 --no-banner ${imagePath}`, async err => {
   console.log(response.text)
   const { leftDoorOpen, rightDoorOpen, leftCarParked, rightCarParked } = JSON.parse(response.text)
   const payload = {
-    image: base64ImageFile,
+    image: currentSnapshot,
     leftDoorOpen,
     rightDoorOpen,
     leftCarParked,
     rightCarParked
   }
 
+  /*const leftDoorOpen = true
+  const rightDoorOpen = false
+  const leftCarParked = false
+  const rightCarParked = true
+  const currentSnapshot = readFileSync('/tmp/garage.jpg', { encoding: 'base64' })
+  const payload = { leftDoorOpen, rightDoorOpen, leftCarParked, rightCarParked, image: currentSnapshot }*/
   const alg = 'RS256'
-  const privateKey = await importPKCS8(readFileSync('private.key').toString(), alg)
+  const privateKey = await importPKCS8(readFileSync(`${workingDirectory}/private.key`).toString(), alg)
   const jwt = await new SignJWT({ prop: 'value' })
     .setProtectedHeader({ alg })
     .setIssuedAt()
     .setExpirationTime('1 minute')
     .sign(privateKey)
 
-  await axios.post('https://nagelberg.dev/api/garage', payload, { headers: { Authorization: `Bearer ${jwt}` } })
+  const { data } = await axios.post(`https://${host}/api/garage`, payload, {
+    headers: { Authorization: `Bearer ${jwt}` }
+  })
+
+  if (leftDoorOpen && !leftCarParked && data.leftDoorOpen) {
+    console.log('Left door open and no car parked!')
+    sendNotification('Left door open and no car parked!', currentSnapshot)
+  }
+  if (rightDoorOpen && !rightCarParked && data.rightDoorOpen) {
+    console.log('Right door open and no car parked!')
+    sendNotification('Right door open and no car parked!', currentSnapshot)
+  }
 })
+//}
+//run()
+
+// TODO: instead of reading last value, set a timeout and run again in 5 minutes (if still open and no car, send notification)
---
+++ src/webcam.mjs
@@ -0,0 +1,21 @@
+import { exec } from 'node:child_process'
+import { readFileSync } from 'node:fs'
+import { importPKCS8, SignJWT } from 'jose'
+import axios from 'axios'
+import { workingDirectory } from './config.mjs'
+
+const imagePath = '/tmp/webcam.jpg'
+
+exec(`fswebcam -r 1280x720 --no-banner ${imagePath}`, async err => {  
+  const alg = 'RS256'
+  const privateKey = await importPKCS8(readFileSync(`${workingDirectory}/private.key`).toString(), alg)
+  const jwt = await new SignJWT({ prop: 'value' })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime('1 minute')
+    .sign(privateKey)
+  const payload = { image: readFileSync(imagePath, { encoding: 'base64' }) }
+  await axios.post(`https://${host}/api/garage`, payload, {
+    headers: { Authorization: `Bearer ${jwt}` }
+  })
+})
